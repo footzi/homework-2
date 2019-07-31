75b8c1338dbccabb3d626e39ffc2ed075e45cf19
Vladislav Manakov <footzi@mail.ru>
2019-07-31 21:48:30 +0300

    Add autoscroll and check empty message

--- src/components/Chat/Chat.js
+++ src/components/Chat/Chat.js
@@ -12,10 +12,18 @@ export default class Chat extends Component {
   };
 
   sendMessageOnEnter = e => {
+    if (!this.state.messageInput) {
+      return;
+    }
+
     if (e.key === 'Enter') {
       this.setState({
         messages: [...this.state.messages, { text: this.state.messageInput }],
         messageInput: ''
+      }, () => {
+        if (this.refs.messages) {
+          this.refs.messages.scrollIntoView(false);
+        }
       });
     }
   };
@@ -24,7 +32,7 @@ export default class Chat extends Component {
     return (
       <div className="chat">
         <div className="message-list">
-          <div className="messages">
+          <div className="messages" ref={'messages'}>
             {this.state.messages.map((item, index) => (
               <Message key={index} text={item.text} />
             ))}
